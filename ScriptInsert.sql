3230714929b5d63fd1363d9a0ceaa0baf4699960
Guilherme Feitoza <guilhermefeitoza09@outlook.com>
2022-05-20 23:42:10 -0300

    adição da sln

---
+++ ScriptInsert.sql
@@ -0,0 +1,39 @@
+USE [AppChat]
+GO
+
+INSERT INTO [dbo].[tbLogin]
+           ([Nome]
+           ,[Senha]
+           ,[Ativo])
+     VALUES
+           ('guilherme.feitoza',
+           'unip2022',
+          'S')
+GO
+USE [AppChat]
+GO
+
+
+GO
+
+INSERT INTO [dbo].[tbLogin]
+           ([Nome]
+           ,[Senha]
+           ,[Ativo])
+     VALUES
+           ('guilherme.oliveira',
+           'unip2022',
+          'S')
+GO
+
+
+
+INSERT INTO [dbo].[tbLogin]
+           ([Nome]
+           ,[Senha]
+           ,[Ativo])
+     VALUES
+           ('admin',
+           'unip2022',
+          'S')
+GO
